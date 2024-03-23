e6eafdcf6860caaeab03a28f66b08e0064d9a5ea
Gerson Garayar <garayargerson20@gmail.com>
2024-03-23 11:12:03 -0500

    fix: agregando animacion de pulsacion al boton de whatsapp

--- resources/views/components/w.blade.php
+++ resources/views/components/w.blade.php
@@ -1,4 +1,31 @@
-<button id="btn-w" class="size-16 rounded-full shadow-lg bg-green-500 flex items-center justify-center fixed bottom-4 right-4">
+<div class="pulse size-16 rounded-full bg-green-500 fixed bottom-8 right-8 z-40">
+</div>
+
+
+<style>
+  .uno {
+    animation: uno .2s 30s both;
+  }
+
+  @keyframes uno {
+    from {
+      opacity: 0;
+    }
+  }
+
+  .pulse {
+    animation: pulse-animate 1s infinite;
+  }
+
+  @keyframes pulse-animate {
+    to {
+      transform: scale(1.5);
+      opacity: 0;
+    }
+  }
+</style>
+
+<button id="btn-w" class="size-16 rounded-full shadow-lg bg-green-500 flex items-center justify-center fixed z-50 bottom-8 right-8">
   <div class="w-icon">
     <x-w-icon />
   </div>
@@ -7,7 +34,9 @@
   </div>
 </button>
 
-<section id="ws" class="bg-white w-96 rounded-xl shadow-lg fixed bottom-24 right-4 overflow-hidden hidden">
+<div class="uno size-4 flex justify-center items-center rounded-full bg-red-500 fixed z-50 pointer-events-none bottom-20 right-8 text-white text-xs">1</div>
+
+<section id="ws" class="bg-white w-96 rounded-xl shadow-lg fixed bottom-28 right-8 overflow-hidden hidden">
   <header class="bg-green-500 px-4 py-6 flex items-center gap-4">
     <div>
       <x-w-icon />
@@ -47,6 +76,7 @@
   const ws = document.getElementById("ws")
   const wIcon = document.querySelector(".w-icon")
   const xIcon = document.querySelector(".x-icon")
+  const uno = document.querySelector(".uno")
 
   btnW.addEventListener("click", () => {
 
@@ -58,6 +88,7 @@
       ws.setAttribute('data-ws', '');
       wIcon.style.display = "none"
       xIcon.style.display = "block"
+      uno.style.display = "none"
     }
 
   })
